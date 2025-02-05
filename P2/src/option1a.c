2712cbbcb0cb3dfd91c9e2f6171d5908642a3f00
Kristian Sørdal <krisor99@gmail.com>
2025-02-05 13:28:22 +0100

    asd

--- P2/src/option1a.c
+++ P2/src/option1a.c
@@ -31,6 +31,7 @@ int main(int argc, char **argv) {
         printf("Done partitioning graph\n");
     }
 
+    MPI_Barrier(MPI_COMM_WORLD);
     if (rank == 0)
         printf("Distributing graph\n");
 
