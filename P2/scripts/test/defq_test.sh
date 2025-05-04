4d37da976651090c659586fb6a137933022eef88
Kristian Sørdal <krisor99@gmail.com>
2025-05-04 15:37:56 +0200

    asdf

--- P2/scripts/test/defq_test.sh
+++ P2/scripts/test/defq_test.sh
@@ -3,24 +3,27 @@ GROUP_FILE="../output.txt"
 
 if [[ -f "$GROUP_FILE" && -r "$GROUP_FILE" ]]; then
     while IFS= read -r line; do
-        sh launch$1.sh $2 "$line" 1 1 64 
-        sh launch$1.sh $2 "$line" 2 1 64 
-        sh launch$1.sh $2 "$line" 3 1 64
-        sh launch$1.sh $2 "$line" 4 1 64 
+        sh launch$1.sh $2 "$line" 1 1 1 
+        sh launch$1.sh $2 "$line" 1 2 1 
+        sh launch$1.sh $2 "$line" 1 4 1
+        sh launch$1.sh $2 "$line" 1 8 1 
+        sh launch$1.sh $2 "$line" 1 16 1 
+        sh launch$1.sh $2 "$line" 1 32 1 
+        sh launch$1.sh $2 "$line" 1 64 1 
     done < "$GROUP_FILE"
 else
     echo "Error: File '$GROUP_FILE' does not exist or is not readable."
     exit 1
 fi
 
-if [[ -f "$GROUP_FILE" && -r "$GROUP_FILE" ]]; then
-    while IFS= read -r line; do
-        sh launch$1.sh $2 "$line" 1 2 32
-        sh launch$1.sh $2 "$line" 2 2 32
-        sh launch$1.sh $2 "$line" 3 2 32
-        sh launch$1.sh $2 "$line" 4 2 32 
-    done < "$GROUP_FILE"
-else
-    echo "Error: File '$GROUP_FILE' does not exist or is not readable."
-    exit 1
-fi
+#if [[ -f "$GROUP_FILE" && -r "$GROUP_FILE" ]]; then
+    #while IFS= read -r line; do
+        #sh launch$1.sh $2 "$line" 1 2 32
+        #sh launch$1.sh $2 "$line" 2 2 32
+        #sh launch$1.sh $2 "$line" 3 2 32
+        #sh launch$1.sh $2 "$line" 4 2 32 
+    #done < "$GROUP_FILE"
+#else
+    #echo "Error: File '$GROUP_FILE' does not exist or is not readable."
+    #exit 1
+#fi
--- P2/scripts/test/fpgaq_test.sh
+++ P2/scripts/test/fpgaq_test.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-GROUP_FILE="/home/krisor99/SpMV-Comm-Strats/P2/scripts/Lynx.txt"
+GROUP_FILE="../output.txt"
 
 if [[ -f "$GROUP_FILE" && -r "$GROUP_FILE" ]]; then
     while IFS= read -r line; do
-        sh launch$1.sh $2 "$line" 1 1 1
-        sh launch$1.sh $2 "$line" 1 1 2
-        sh launch$1.sh $2 "$line" 1 1 4
-        sh launch$1.sh $2 "$line" 1 1 8
-        sh launch$1.sh $2 "$line" 1 1 16
-        sh launch$1.sh $2 "$line" 1 1 32
         sh launch$1.sh $2 "$line" 1 1 48
+        sh launch$1.sh $2 "$line" 2 1 48
+        sh launch$1.sh $2 "$line" 3 1 48
+        sh launch$1.sh $2 "$line" 4 1 48
     done < "$GROUP_FILE"
 else
     echo "Error: File '$GROUP_FILE' does not exist or is not readable."
@@ -19,13 +16,10 @@ fi
 
 if [[ -f "$GROUP_FILE" && -r "$GROUP_FILE" ]]; then
     while IFS= read -r line; do
-        sh launch$1.sh $2 "$line" 1 1 1
-        sh launch$1.sh $2 "$line" 1 2 1
-        sh launch$1.sh $2 "$line" 1 4 1
-        sh launch$1.sh $2 "$line" 1 8 1
-        sh launch$1.sh $2 "$line" 1 16 1
-        sh launch$1.sh $2 "$line" 1 32 1
-        sh launch$1.sh $2 "$line" 1 48 1
+        sh launch$1.sh $2 "$line" 1 2 48
+        sh launch$1.sh $2 "$line" 2 2 48
+        sh launch$1.sh $2 "$line" 3 2 48
+        sh launch$1.sh $2 "$line" 4 2 48
     done < "$GROUP_FILE"
 else
     echo "Error: File '$GROUP_FILE' does not exist or is not readable."
