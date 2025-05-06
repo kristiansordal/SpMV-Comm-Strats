a536bdda7da1b932d3bcd8974165a497acec4d8d
Kristian Sørdal <krisor99@gmail.com>
2025-05-06 09:43:32 +0200

    aasdf

--- P2/scripts/test/sbatch_script.sh
+++ P2/scripts/test/sbatch_script.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-#SBATCH --partition=fpgaq
-#SBATCH --nodes=4
-#SBATCH --ntasks-per-node=2
-#SBATCH --cpus-per-task=48
-#SBATCH --job-name=1a_bone010_4_nodes_2_tasks_48_threads_mpi
+#SBATCH --partition=rome16q
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --job-name=1a_bone010_8_nodes_1_tasks_16_threads
 #SBATCH --distribution=block:block
 #SBATCH --exclusive
 #SBATCH --time=0-0:10:00
-#SBATCH --output=/home/krisor99/SpMV-Comm-Strats/P2/results/multi/fpgaq/%x-%j-stdout.txt
-#SBATCH --error=/home/krisor99/SpMV-Comm-Strats/P2/results/multi/fpgaq/%x-%j-stderr.txt
+#SBATCH --output=/home/krisor99/SpMV-Comm-Strats/P2/results/multi/rome16q/%x-%j-stdout.txt
+#SBATCH --error=/home/krisor99/SpMV-Comm-Strats/P2/results/multi/rome16q/%x-%j-stderr.txt
 
 module load openmpi-4.1.6
 module load cmake-3.22.3
 export LC_ALL=C
+export OMPI_MCA_pml=ucx
+export OMPI_MCA_btl=self,vader,tcp
+export OMPI_MCA_btl_tcp_if_exclude=lo,eno1,eno2,docker0,docker_gwbridge
+export UCX_TLS=rc,ud,self
+export UCX_NET_DEVICES=mlx5_1:1
+export OMP_NUM_THREADS=16
 
-srun --verbose numactl -C0-95 /home/krisor99/SpMV-Comm-Strats/P2/build/Debug/1a /global/D1/projects/mtx/datasets/suitesparse/Oberwolfach/bone010/bone010.mtx
+srun --verbose numactl -C0-15 /home/krisor99/SpMV-Comm-Strats/P2/build/Debug/1a /global/D1/projects/mtx/datasets/suitesparse/Oberwolfach/bone010/bone010.mtx
