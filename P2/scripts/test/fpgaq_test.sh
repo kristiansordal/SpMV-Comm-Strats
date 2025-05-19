ccc5aad392647b9c0465ea20a4b4d815efab25c5
Kristian Sørdal <krisor99@gmail.com>
2025-05-19 12:45:25 +0200

    asdf

--- P2/scripts/test/fpgaq_test.sh
+++ P2/scripts/test/fpgaq_test.sh
@@ -3,25 +3,25 @@
 GROUP_FILE="../output.txt"
 
 if [[ -f "$GROUP_FILE" && -r "$GROUP_FILE" ]]; then
-    while IFS= read -r line; do
-        sh launch$1.sh $2 "$line" 1 1 48
-        sh launch$1.sh $2 "$line" 2 1 48
-        sh launch$1.sh $2 "$line" 3 1 48
-        sh launch$1.sh $2 "$line" 4 1 48
-    done < "$GROUP_FILE"
+   while IFS= read -r line; do
+       sh launch$1.sh $2 "$line" 1 1 48
+       sh launch$1.sh $2 "$line" 2 1 48
+       sh launch$1.sh $2 "$line" 3 1 48
+       sh launch$1.sh $2 "$line" 4 1 48
+   done < "$GROUP_FILE"
 else
-    echo "Error: File '$GROUP_FILE' does not exist or is not readable."
-    exit 1
+   echo "Error: File '$GROUP_FILE' does not exist or is not readable."
+   exit 1
 fi
 
-if [[ -f "$GROUP_FILE" && -r "$GROUP_FILE" ]]; then
-    while IFS= read -r line; do
-        sh launch$1.sh $2 "$line" 1 2 48
-        sh launch$1.sh $2 "$line" 2 2 48
-        sh launch$1.sh $2 "$line" 3 2 48
-        sh launch$1.sh $2 "$line" 4 2 48
-    done < "$GROUP_FILE"
-else
-    echo "Error: File '$GROUP_FILE' does not exist or is not readable."
-    exit 1
-fi
+#if [[ -f "$GROUP_FILE" && -r "$GROUP_FILE" ]]; then
+#    while IFS= read -r line; do
+#        sh launch$1.sh $2 "$line" 1 2 24
+#        sh launch$1.sh $2 "$line" 2 2 24
+#        sh launch$1.sh $2 "$line" 3 2 24
+#        sh launch$1.sh $2 "$line" 4 2 24
+#    done < "$GROUP_FILE"
+#else
+#    echo "Error: File '$GROUP_FILE' does not exist or is not readable."
+#    exit 1
+#fi
--- P2/scripts/test/sbatch_script.sh
+++ P2/scripts/test/sbatch_script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --partition=defq
+#SBATCH --partition=fpgaq
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=2
-#SBATCH --cpus-per-task=32
-#SBATCH --job-name=1a_bone010_4_nodes_2_tasks_32_threads_mpi
+#SBATCH --cpus-per-task=24
+#SBATCH --job-name=1a_bone010_4_nodes_2_tasks_24_threads_mpi
 #SBATCH --distribution=block:block
 #SBATCH --exclusive
 #SBATCH --time=0-0:10:00
-#SBATCH --output=/home/krisor99/SpMV-Comm-Strats/P2/results/multi/defq/%x-%j-stdout.txt
-#SBATCH --error=/home/krisor99/SpMV-Comm-Strats/P2/results/multi/defq/%x-%j-stderr.txt
+#SBATCH --output=/home/krisor99/SpMV-Comm-Strats/P2/results/multi/fpgaq/%x-%j-stdout.txt
+#SBATCH --error=/home/krisor99/SpMV-Comm-Strats/P2/results/multi/fpgaq/%x-%j-stderr.txt
 
 module load openmpi-4.1.6
 module load cmake-3.22.3
@@ -18,7 +18,7 @@ export OMPI_MCA_pml=ucx
 export OMPI_MCA_btl=self,vader,tcp
 export OMPI_MCA_btl_tcp_if_exclude=lo,eno1,eno2,docker0,docker_gwbridge
 export UCX_TLS=rc,ud,self
-export UCX_NET_DEVICES=mlx5_2:1
-export OMP_NUM_THREADS=32
+export UCX_NET_DEVICES=mlx5_4:1
+export OMP_NUM_THREADS=24
 
-srun --verbose numactl -C0-63 /home/krisor99/SpMV-Comm-Strats/P2/build/defq/1a /global/D1/projects/mtx/datasets/suitesparse/Oberwolfach/bone010/bone010.mtx
+srun --verbose numactl -C0-47 /home/krisor99/SpMV-Comm-Strats/P2/build/fpgaq/1a /global/D1/projects/mtx/datasets/suitesparse/Oberwolfach/bone010/bone010.mtx
