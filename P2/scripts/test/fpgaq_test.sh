6797ba7aaf0e4db1f2feb310126c355e45794bd1
Kristian Sørdal <krisor99@gmail.com>
2025-05-28 12:52:19 +0200

    asdf

--- P2/scripts/test/fpgaq_test.sh
+++ P2/scripts/test/fpgaq_test.sh
@@ -8,7 +8,7 @@ if [[ -f "$GROUP_FILE" && -r "$GROUP_FILE" ]]; then
        sh launch$1.sh $2 "$line" 2 1 48
        sh launch$1.sh $2 "$line" 3 1 48
        sh launch$1.sh $2 "$line" 4 1 48
-   done < "$GROUP_FILE"
+  done < "$GROUP_FILE"
 else
    echo "Error: File '$GROUP_FILE' does not exist or is not readable."
    exit 1
@@ -20,7 +20,7 @@ if [[ -f "$GROUP_FILE" && -r "$GROUP_FILE" ]]; then
         sh launch$1.sh $2 "$line" 2 2 24
         sh launch$1.sh $2 "$line" 3 2 24
         sh launch$1.sh $2 "$line" 4 2 24
-    done < "$GROUP_FILE"
+   done < "$GROUP_FILE"
 else
     echo "Error: File '$GROUP_FILE' does not exist or is not readable."
     exit 1
